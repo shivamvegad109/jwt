a45b905af9dbf057f8fe80610cf639644268c87e
shivamvegad109 <shivamvegad109@gmail.com>
2023-08-18 15:20:02 +0530

    password

--- myproject/settings.py
+++ myproject/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "simple_jwt",
         "USER": "postgres",
-        "PASSWORD": "root",
+        "PASSWORD": "1234",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
